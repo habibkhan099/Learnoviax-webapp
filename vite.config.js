b9b949bc72d6a261f52bf60ea27ca81672e86143
habibkhan099 <hbh90619@gmail.com>
2025-09-16 01:25:34 +0500

    Fix blank page issue for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Learnoviax-webapp",
-})
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+  },
+  base: "./", // ✅ fix blank page on Vercel
+});
